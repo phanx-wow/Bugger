87bfea2498b603560bd8d041a1f4ab175ea2fe9b
Phanx-28751 <addons@phanx.net>
2014-10-04 04:40:11 +0000

    - Update with WoWI URL

--- Localization.lua
+++ Localization.lua
@@ -1,3 +1,12 @@
+--[[--------------------------------------------------------------------
+	Bugger
+	Basic GUI front-end for !BugGrabber.
+	Copyright (c) 2014 Phanx. All rights reserved.
+	See the accompanying README and LICENSE files for more information.
+	http://www.wowinterface.com/downloads/info23144-Bugger.html
+	http://www.curse.com/addons/wow/bugger
+----------------------------------------------------------------------]]
+
 local BUGGER, Bugger = ...
 local L = Bugger.L
 
