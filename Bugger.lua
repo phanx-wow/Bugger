70b2e1f11a1dec2868dbb8a7a19f6917600ca5a2
Phanx <addons@phanx.net>
2014-11-16 05:26:33 -0800

    Post SVN->Git cleanup

--- Bugger.lua
+++ Bugger.lua
@@ -1,16 +1,10 @@
 --[[--------------------------------------------------------------------
 	Bugger
 	Shows the errors captured by !BugGrabber.
+	Copyright (c) 2014 Phanx <addons@phanx.net>. All rights reserved.
 	http://www.wowinterface.com/downloads/info23144-Bugger.html
 	http://www.curse.com/addons/wow/bugger
-
-	Copyright (c) 2014 Phanx <addons@phanx.net>. All rights reserved.
-	Please DO NOT upload this addon to other websites, or post modified
-	versions of it. However, you are welcome to include a copy of it
-	WITHOUT CHANGES in compilations posted on Curse and/or WoWInterface.
-	You are also welcome to use any/all of its code in your own addon, as
-	long as you do not use my name or the name of this addon ANYWHERE in
-	your addon, including its name, outside of an optional attribution.
+	https://github.com/Phanx/Bugger
 ----------------------------------------------------------------------]]
 
 local BUGGER, Bugger = ...
